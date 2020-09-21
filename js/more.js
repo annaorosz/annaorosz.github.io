0dacf9fa454d72dbcf93cfc328b2da26a103134c
Anna Orosz <aorosz@sas.upenn.edu>
2020-09-22 00:24:07 +0200

    new menu button

--- js/more.js
+++ js/more.js
@@ -1,25 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var showChar = 200;
     var ellipsestext = "...";
     var moretext = "more";
     var lesstext = "less";
-    $('.more').each(function() {
+    $('.more').each(function () {
         var content = $(this).html();
 
-        if(content.length > showChar) {
+        if (content.length > showChar) {
 
             var c = content.substr(0, showChar);
-            var h = content.substr(showChar-1, content.length - showChar);
+            var h = content.substr(showChar - 1, content.length - showChar);
 
-            var html = c + '<span class="moreelipses">'+ellipsestext+'</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">'+moretext+'</a></span>';
+            var html = c + '<span class="moreelipses">' + ellipsestext + '</span>&nbsp;<span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
 
             $(this).html(html);
         }
 
     });
 
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
+    $(".morelink").click(function () {
+        if ($(this).hasClass("less")) {
             $(this).removeClass("less");
             $(this).html(moretext);
         } else {
